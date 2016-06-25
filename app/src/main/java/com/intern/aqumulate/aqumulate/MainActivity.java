469e9e2e824aaadd07d6179a2abfc02ae9f3a001
prakash <prakashrawat2@gmail.com>
2016-06-25 11:22:49 +0530

    ok

--- app/src/main/java/com/intern/aqumulate/aqumulate/MainActivity.java
+++ app/src/main/java/com/intern/aqumulate/aqumulate/MainActivity.java
@@ -1,13 +1,24 @@
 package com.intern.aqumulate.aqumulate;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
+       // NavigationDrawerFragment drawerFragment= (NavigationDrawerFragment)
+               // getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+
+     //   drawerFragment.setUp((DrawerLayout) findViewById(R.id.drawer_layout),toolbar);
     }
 }
---
+++ app/src/main/java/com/intern/aqumulate/aqumulate/NavigationDrawerFragment.java
@@ -0,0 +1,35 @@
+package com.intern.aqumulate.aqumulate;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NavigationDrawerFragment extends Fragment {
+
+   private ActionBarDrawerToggle mDrawerToggle;
+
+
+    public NavigationDrawerFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+    }
+
+    public void setUp(DrawerLayout drawer) {
+    }
+}
