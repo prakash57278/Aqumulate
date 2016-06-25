8a7b8cf597267e095cc4e2ac2944c9eea8814ba8
prakash <prakashrawat2@gmail.com>
2016-06-25 12:51:41 +0530

    navDrawer

--- app/src/main/java/com/intern/aqumulate/aqumulate/MainActivity.java
+++ app/src/main/java/com/intern/aqumulate/aqumulate/MainActivity.java
@@ -1,8 +1,8 @@
 package com.intern.aqumulate.aqumulate;
 
+import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,15 +10,15 @@ public class MainActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_appbar);
 
 
         toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationDrawerFragment drawerFragment= (NavigationDrawerFragment)
+                getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
 
-       // NavigationDrawerFragment drawerFragment= (NavigationDrawerFragment)
-               // getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
-
-     //   drawerFragment.setUp((DrawerLayout) findViewById(R.id.drawer_layout),toolbar);
+       drawerFragment.setUp(R.id.fragment_navigation_drawer,(DrawerLayout) findViewById(R.id.drawer_layout),toolbar);
     }
 }
