27ad7545a40ffeaead644871dc919d985409dbf5
prakash <prakashrawat2@gmail.com>
2016-06-25 20:56:28 +0530

    navDrawer

---
+++ app/src/main/java/com/intern/aqumulate/aqumulate/AssestsLiabilities.java
@@ -0,0 +1,13 @@
+package com.intern.aqumulate.aqumulate;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AssestsLiabilities extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assests_liabilities);
+    }
+}
